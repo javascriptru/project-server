0683eb1810dfdc03a6e8228991a25f899af91258
Ilya Kantor <iliakan@gmail.com>
2020-03-05 14:20:06 +0000

    minor fixes

--- tasks/server.js
+++ tasks/server.js
@@ -2,8 +2,11 @@
 module.exports = function(app) {
   return async function() {
 
-    let server = app.listen(process.env.PORT || 80, process.env.HOST || '0.0.0.0', () => {
-      console.log(`App is running on port ${config.server.port}`);
+    let port = process.env.PORT || 80;
+    let host = process.env.HOST || '0.0.0.0';
+    
+    let server = app.listen(port, host, () => {
+      console.log(`App is running on ${host}:${port}`);
     });
 
     await new Promise(res => server.on('close', res));
